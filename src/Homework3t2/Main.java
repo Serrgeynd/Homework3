2ecba92e409f1b34d1996fd30bfa33da46ada9ed
sdudin <serrgeynd@rambler.ru>
2020-10-02 22:37:15 +0300

    Домашнее задание 3 Добавил Main

---
+++ src/Homework3t2/Main.java
@@ -0,0 +1,16 @@
+package Homework3t2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Veterinary veterinary = new Veterinary();
+        Animal[] animal = new Animal[3];
+        animal[0] = new Cat("Мясо", "Квартира 22", "Царапается");
+        animal[1] = new Dog("Кости", "Конура", "Линяет");
+        animal[2] = new Horse("Сено", "Конюшня", "Быстро бегает");
+        veterinary.treatAnimal(animal[1]);
+
+
+    }
+}
