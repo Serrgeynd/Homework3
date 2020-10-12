9b67cc25a029b372a7f6e7fdc1881c991a1b947a
sdudin <serrgeynd@rambler.ru>
2020-10-12 22:54:54 +0300

    Убрал инициализацию переменных

--- src/Homework3t2/Veterinary.java
+++ src/Homework3t2/Veterinary.java
@@ -3,10 +3,6 @@ package Homework3t2;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 
 public class Veterinary {
-
-    Animal animal = new Animal("", "");
-
-
     public void treatAnimal(Animal animal) {
         System.out.println("Ест:  " + animal.food + "  Живет:  " + animal.location);
 
